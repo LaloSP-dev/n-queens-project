b7c8d4159b96e6425a0902df672a2a6f95b621a4
JoseTDJ <jose.alf.tre@gmail.com>
2023-06-16 22:35:24 -0600

    update 1 TaBleroJ.py

--- TaBleroJ.py
+++ TaBleroJ.py
@@ -7,40 +7,55 @@ raiz = Tk()
 
 # Crea una forma
 frm = ttk.Frame(raiz)
-frm.grid(columnspan=8,rowspan=8)
+frm.grid(columnspan=3,rowspan=3)
 #########################################Jose###################33
-# Definir dimensiones del tablero
-dimension = 9
-casilla_size = 70
+casillas = []
 
-# Crea una matriz para representar el tablero
-tablero = [[None] * dimension for _ in range(dimension)]
+def dibujar_tablero(num):
+    # Definir dimensiones del tablero
+    #dimension = 9
+    casilla_size = 70
 
-# Colores para las casillas
-color1 = "red"
-color2 = "black"
+    # Crea una matriz para representar el tablero
+    #tablero = [[None] * dimension for _ in range(dimension)]
+    tablero = [[None] * num for _ in range(num)]
 
-# Dibuja el tablero
-for fila in range(dimension):
-    for columna in range(dimension):
-        # Alterna los colores de las casillas
-        if (fila + columna) % 2 == 0:
-            color = color1
-        else:
-            color = color2
+    # Colores para las casillas
+    color1 = "red"
+    color2 = "black"
 
-        # Crea una casilla como un marco
-        casilla = ttk.Frame(frm, width=casilla_size, height=casilla_size, background=color)
-        casilla.grid(row=fila, column=columna)
+    # Dibuja el tablero
+    for fila in range(num):
+        for columna in range(num):
+            # Alterna los colores de las casillas
+            if (fila + columna) % 2 == 0:
+                color = color1
+            else:
+                color = color2
 
-        # Agrega la casilla al tablero
-        tablero[fila][columna] = casilla
+            # Crea una casilla como un marco
+            casilla = ttk.Frame(frm, width=casilla_size, height=casilla_size, background=color)
+            casillas.append(casilla)
+            casilla.grid(row=fila, column=columna)
+
+            # Agrega la casilla al tablero
+            tablero[fila][columna] = casilla
+   
+    # Pon una imagen en una casilla específica (fila, columna)
+    def colocar_imagen(fila, columna):
+        ttk.Label(tablero[fila][columna], image=img).pack()
+
+    # Ejemplo: Coloca una imagen en la casilla (0, 0)
+    colocar_imagen(1, 0)
+    colocar_imagen(2, 1)
+    colocar_imagen(3, 2)
+
 
 #############################################################################
 
 # Lee imagen
 img=PhotoImage(file='Imagenes/reina.png')
-img=img.subsample(8)
+img=img.subsample(9)
 
 
 
@@ -55,16 +70,7 @@ ttk.Button(frm,text='Minimiza',command=raiz.iconify).grid(column=2, row=5)
 #ttk.Label(frm,image=img).grid(column=3,row=2)
 #ttk.Label(frm,image=img).grid(column=4,row=3)
 #ttk.Label(frm,image=img).grid(column=1,row=4)
-##################################################################33
-# Pon una imagen en una casilla específica (fila, columna)
-def colocar_imagen(fila, columna):
-    ttk.Label(tablero[fila][columna], image=img).pack()
 
-# Ejemplo: Coloca una imagen en la casilla (0, 0)
-colocar_imagen(1, 0)
-colocar_imagen(2, 1)
-colocar_imagen(3, 2)
-#####################################################################
 # Lee algo
 var = IntVar()
 
@@ -78,11 +84,21 @@ def imprime(x):
 
 num.bind('<Key-Return>',imprime)
 
+##############################################3
+def limpiar_tablero():
+    for casilla in casillas:
+        casilla.destroy()
+    casillas.clear()
+###############################################3
+
 # Comando
 def comando():
+    limpiar_tablero()
+    valor = int(num.get())
     print(f"Se dio click, num vale '{num.get()}'")
+    dibujar_tablero(valor)
 
-ttk.Button(frm,text='Da click',command=comando).grid(column=4, row=5)
+ttk.Button(frm,text='Da click',command=comando).grid(column=5, row=6)
 
 # Haz funcionar todo
 raiz.mainloop()
