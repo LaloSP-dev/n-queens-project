0035c65ebf802716dce27392e921cf319c721167
Cesar240899 <cesarin.cg23@gmail.com>
2023-06-17 17:33:56 -0600

    update Serv_Clase

---
+++ servidorClase.pl
@@ -0,0 +1,29 @@
+###################################
+       Servidor
+#######################################
+
+:-consult(queens).
+
+servidor:-
+    write('Empieza servidor'),nl,
+    socket('AF_INET',Socket),
+    socket_bind(Socket, 'AF_INET'(N, 50000))
+    socket_listen(Socket, 5),
+    socket_accept(Socket, ClienteIP, Stream),
+    doService(ClienteIP, Stream),
+    socket_close(Socket).
+
+doService(ClienteIP, Stream):-
+       write('Recibi llamada de : '), write(ClienteIP), nl,
+       % manda varias cadenas
+       repeat,
+              % antes era member [x, [hola,esta,es,una,prueba,fin]].
+              read(Stream,N),
+              (reinasP(N,L), write(Stream, L), put(Stream, 13), put(Stream, 10);
+               write(Stream, no), put(Stream, 13), put(Stream, 10)),
+              flush_output(Stream),
+       N==fin, % aca termina
+       !,
+       write('Adios'), nl,
+       close(Stream).
+       
