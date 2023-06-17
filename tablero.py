2e1f0e5594b5352c24d9b1b3a3dd14509fcfaf56
Eduardo Sanchez Pascual <77649457+LaloSP-dev@users.noreply.github.com>
2023-06-17 00:48:12 -0500

    tablero

--- tablero.py
+++ tablero.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import tkinter as tk
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter.tix import IMAGETEXT
 
 class TableroAjedrez(tk.Frame):
 
@@ -24,12 +27,21 @@ class TableroAjedrez(tk.Frame):
 
 def mostrar_tablero():
     n = int(entry.get())  # Obtener el tamaño del tablero desde la entrada
-    # Limpiar el contenido del marco del tablero
-    for widget in tablero_frame.winfo_children():
-        widget.destroy()
-    tablero = TableroAjedrez(tablero_frame, n)
-    tablero.crear_tablero()
-    tablero.pack(expand=True, fill="both")
+    if n < 4:
+        messagebox.showerror("Error", "El tamaño mínimo del tablero es 4.")
+    elif n > 15:
+        messagebox.showerror("Error", "El tamaño máximo del tablero es 15.")
+    else:
+        # Limpiar el contenido del marco del tablero
+        for widget in tablero_frame.winfo_children():
+            widget.destroy()
+        
+        # Destruir el widget de la imagen
+        imagen_label.destroy()
+
+        tablero = TableroAjedrez(tablero_frame, n)
+        tablero.crear_tablero()
+        tablero.pack(expand=True, fill="both")
 
 def cerrar_aplicacion():
     root.destroy()
@@ -40,12 +52,12 @@ def minimizar_ventana():
 
 root = tk.Tk()
 root.geometry("500x500")  # Tamaño inicial de la ventana
-root.title("Tablero de Ajedrez")
+root.title("Problema de las N Reinas")
 
 frame = tk.Frame(root)
 frame.pack(expand=True)
 
-label = tk.Label(frame, text="Ingrese el tamaño del tablero (n):")
+label = tk.Label(frame, text="Ingrese el numero de Reinas:")
 label.pack()
 
 entry = tk.Entry(frame)
@@ -60,12 +72,24 @@ tablero_frame.pack(expand=True)
 tablero_frame.grid_rowconfigure(0, weight=1)
 tablero_frame.grid_columnconfigure(0, weight=1)
 
-# Agregar botones estáticos con separación
-button1 = tk.Button(frame, text="Cerrar", command=cerrar_aplicacion)
+# Cargar la imagen y ajustar su tamaño
+imagen = Image.open("Imagenes/chees.png")
+imagen = imagen.resize((400, 200), Image.ANTIALIAS)  # Ajustar el tamaño de la imagen
+imagen = ImageTk.PhotoImage(imagen)
+
+# Mostrar la imagen en un widget Label
+imagen_label = tk.Label(frame, image=imagen)
+imagen_label.pack(pady=10) 
+
+# Agregar un marco para contener los botones
+botones_frame = tk.Frame(frame)
+botones_frame.pack()
+
+# Agregar botones estáticos en el marco de los botones
+button1 = tk.Button(botones_frame, text="Cerrar", command=cerrar_aplicacion)
 button1.pack(side=tk.LEFT, padx=10)
 
-button2 = tk.Button(frame, text="Minimizar", command=minimizar_ventana)
+button2 = tk.Button(botones_frame, text="Minimizar", command=minimizar_ventana)
 button2.pack(side=tk.LEFT, padx=10)
-
 
 root.mainloop()
